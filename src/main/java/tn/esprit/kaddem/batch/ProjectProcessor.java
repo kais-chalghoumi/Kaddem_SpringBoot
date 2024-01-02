ec115fb317907172b11615d9318c20d016c47657
kais-chalghoumi <kais.chalghoumi@esprit.tn>
2024-01-02 14:11:21 +0100

    restructure

--- src/main/java/tn/esprit/kaddem/batch/ProjectProcessor.java
+++ src/main/java/tn/esprit/kaddem/batch/ProjectProcessor.java
@@ -1,23 +1,21 @@
-package tn.esprit.kaddem.batch;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.item.ItemProcessor;
-import tn.esprit.kaddem.entities.Equipe;
-import tn.esprit.kaddem.entities.Etudiant;
-
-@Slf4j
-public class ProjectProcessor implements ItemProcessor<Equipe, Equipe> {
-	/*12. logique métier de notre job*/
-	@Override
-	public Equipe process(Equipe equipe) {
-		log.info("Start Batch Item Processor");
-		for (Etudiant etudiant : equipe.getEtudiants() ) {
-			etudiant.getContrats().forEach(contrat -> {
-				contrat.setMontantContrat((int) (contrat.getMontantContrat()+(contrat.getMontantContrat()*0.1)));
-				//todo3  (mettre à jour le montant du contrat avec une augmentation de 10%)
-			});
-
-		}
-		return equipe;
-	}
-}
+//package tn.esprit.kaddem.batch;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.batch.item.ItemProcessor;
+//import tn.esprit.kaddem.entities.Equipe;
+//import tn.esprit.kaddem.entities.Etudiant;
+//
+//@Slf4j
+//public class ProjectProcessor implements ItemProcessor<Equipe, Equipe> {
+//	@Override
+//	public Equipe process(Equipe equipe) {
+//		log.info("Start Batch Item Processor");
+//		for (Etudiant etudiant : equipe.getEtudiants() ) {
+//			etudiant.getContrats().forEach(contrat -> {
+//				contrat.setMontantContrat((int) (contrat.getMontantContrat()+(contrat.getMontantContrat()*0.1)));
+//			});
+//
+//		}
+//		return equipe;
+//	}
+//}
--- src/main/java/tn/esprit/kaddem/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/kaddem/services/DepartementServiceImpl.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Set;
 
 @Slf4j
-
 @Service
 public class DepartementServiceImpl implements IDepartementService{
 
